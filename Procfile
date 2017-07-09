34c468c447cf86ccd7816f2014a2aec9cf763ce3
docbtc <doug@bitstop.co>
2017-07-09 13:03:41 -0400

    Intial commit
    txt coinbase price via sms

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python satoshipricesms.py
---
+++ satoshipricesms.py
@@ -0,0 +1,27 @@
+import json
+import urllib2
+import time
+from twilio.rest import TwilioRestClient
+import os
+
+
+ACCOUNT_SID = os.environ['TWILIO_ID']  
+AUTH_TOKEN = os.environ['TWILIO_AUTH'] 
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
+
+coinbase_price_data = json.load(urllib2.urlopen("https://api.coinbase.com/v2/prices/BTC-USD/spot/"))
+usd_bitcoin_price = float(coinbase_price_data['data']['amount'])
+
+
+print "The price is : " + str(usd_bitcoin_price)
+
+print 'Sending Price'
+			client.messages.create(
+				to=os.environ['PHONE_TO'],
+				from_=os.environ['PHONE_FROM'], 
+				body="The current bitcoin price is : " + " " + "1 BTC:" + str(usd_bitcoin_price) + "USD"
+
+
+time.sleep(5 * 60)
