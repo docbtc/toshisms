b53ad07b07c0f419ced69734ea808fd882caa887
docbtc <doug@bitstop.co>
2017-07-09 14:34:19 -0400

    try again

--- satoshipricesms.py
+++ satoshipricesms.py
@@ -1,26 +1,24 @@
 import json
 from urllib.request import urlopen
 import time
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 import os
 
 
 ACCOUNT_SID = os.environ['TWILIO_ID']  
 AUTH_TOKEN = os.environ['TWILIO_AUTH'] 
 
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 coinbase_price_data = json.load(urlopen("https://api.coinbase.com/v2/prices/BTC-USD/spot/"))
 usd_bitcoin_price = float(coinbase_price_data['data']['amount'])
 
 print ("The price is: " + str(usd_bitcoin_price)
 
-message=client.messages.create(
+message = client.messages.create(
 	to=os.environ['PHONE_TO'],
 	from_=os.environ['PHONE_FROM'], 
 	body=("The current bitcoin price is :" + " " + "1 BTC:" + str(usd_bitcoin_price) + "USD")
 	)
 	
-print(message.sid)
-
 time.sleep(1 * 60)
