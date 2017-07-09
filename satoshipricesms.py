e5376751c425485a4e73691c9f4321ace3f3da87
docbtc <doug@bitstop.co>
2017-07-09 13:53:22 -0400

    python 3 shit?

--- satoshipricesms.py
+++ satoshipricesms.py
@@ -13,7 +13,7 @@ client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 coinbase_price_data = json.load(urllib2.urlopen("https://api.coinbase.com/v2/prices/BTC-USD/spot/"))
 usd_bitcoin_price = float(coinbase_price_data['data']['amount'])
 
-print 'Sending Price'
+print ('Sending Price')
 			client.messages.create(
 				to=os.environ['PHONE_TO'],
 				from_=os.environ['PHONE_FROM'], 
