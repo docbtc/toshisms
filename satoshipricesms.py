0531933899e0b844bc98e81fe3c56de823c1238f
docbtc <doug@bitstop.co>
2017-07-09 21:04:43 -0400

    spacing

--- satoshipricesms.py
+++ satoshipricesms.py
@@ -12,16 +12,17 @@ client = Client('ACCOUNT_SID', 'AUTH_TOKEN')
 
 
 if __name__ == "__main__":
-coinbase_price_data = json.load(urlopen("https://api.coinbase.com/v2/prices/BTC-USD/spot/"))
-usd_bitcoin_price = float(coinbase_price_data['data']['amount'])
 
-print("The price is: " + str(usd_bitcoin_price))
+	coinbase_price_data = json.load(urlopen("https://api.coinbase.com/v2/prices/BTC-USD/spot/"))
+	usd_bitcoin_price = float(coinbase_price_data['data']['amount'])
 
-myMessage = client.messages.create(
+	print("The price is: " + str(usd_bitcoin_price))
+
+	myMessage = client.messages.create(
 	to=os.environ['PHONE_TO'],
 	from_=os.environ['PHONE_FROM'], 
 	body="The current bitcoin price is :" + " " + "1 BTC:" + str(usd_bitcoin_price) + "USD"
 	)
 
 	
-time.sleep(1 * 60)
+	time.sleep(1 * 60)
